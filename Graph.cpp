975bbc625ecbf3a001c39450620ea258529485b3
Gail Harris <gail.harris@humber.ca>
2024-10-09 16:27:22 -0400

    Week 6-2 live coding, declarations for A* or Dijkstra

--- Graph.cpp
+++ Graph.cpp
@@ -1,4 +1,5 @@
 #include "Graph.h"
+#include <queue>
 
 Graph::Graph()
 {
@@ -60,9 +61,64 @@ std::vector<Node*> Graph::neighbours(Node* fromNode)
     return result;
 }
 
+struct NodeAndPriority
+{
+    Node* node;
+    float priority;
+
+    bool operator()(NodeAndPriority const& lhs, NodeAndPriority const& rhs)
+    {
+        return lhs.priority > rhs.priority;
+    }
+};
+
 std::vector<Node*> Graph::findPath(Node* startNode, Node* goalNode)
 {
+    std::vector<Node*> result;
+
+    //declarations
+    float new_cost;
+    float priority;
+    Node* currentNode = startNode;
+
+    //frontier
+    std::priority_queue<NodeAndPriority, std::deque<NodeAndPriority>, NodeAndPriority > frontier;
+    frontier.push(NodeAndPriority{ currentNode, 0.0f });
+
+    // track solution path (came_from)
+    std::vector<int> came_from;
+    // initialize to -1, a value that cannot be label of a node
+    came_from.resize(numNodes(), -1);
+
+    // cost so far storage
+    // use std::map, not vector, to allow detecting if a node has an entry
+    int start = startNode->getLabel();
+    int goal = goalNode->getLabel();
+
+    std::map<int, float> cost_so_far;
+    cost_so_far[start] = 0.0f;
+
     // TODO
 
-    return std::vector<Node*>();
+    // loop through the frontier, while it is not empty
+    {
+        // get the top node, save it in currentNode
+        // pop the top node
+        // if its the goal, then break out
+
+        // for the neighbours of current node
+        {
+            //calculate new cost
+            // if neighbour is not an index in cost_so_far or new_cost is lower 
+            {
+                // found a better path, so update data structures
+            }
+        }
+    }
+
+    // TODO
+    // follow the breadcrumbs in came_from to rebuild the path, store in result
+
+
+    return result;
 }
